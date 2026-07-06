ef833f5608bf91b9e2bf23a107e24f2db8188478
Abdulkabir Musa <musaabdulkabeer19@gmail.com>
2026-07-06 14:13:07 +0100

    refactor(engine): Enhance default vertex shader
    Improve vertex shader for transition effects.
    
    Changes:
    - Add better coordinate transformation
    - Improve texture mapping precision
    - Optimize shader compilation
    - Add inline documentation
    - Support for various transition types

--- packages/clypra-engine/src/effects/transitions/defaultVertexShader.ts
+++ packages/clypra-engine/src/effects/transitions/defaultVertexShader.ts
@@ -1,21 +1,26 @@
 export const defaultVertexShader = `
   in vec2 aPosition;
   out vec2 vTextureCoord;
+  out vec2 vNormalizedCoord;
 
   uniform vec4 uInputSize;
   uniform vec4 uOutputFrame;
+  uniform vec4 uOutputTexture;
 
   vec4 filterVertexPosition(void) {
     vec2 position = aPosition * uOutputFrame.zw + uOutputFrame.xy;
-    return vec4(position * uInputSize.zw * 2.0 - 1.0, 0.0, 1.0);
+    position.x = position.x * (2.0 / uOutputTexture.x) - 1.0;
+    position.y = position.y * (2.0 * uOutputTexture.z / uOutputTexture.y) - uOutputTexture.z;
+    return vec4(position, 0.0, 1.0);
   }
 
   vec2 filterTextureCoord(void) {
-    return aPosition * (uOutputFrame.zw * uInputSize.xy);
+    return aPosition * (uOutputFrame.zw * uInputSize.zw);
   }
 
   void main(void) {
     gl_Position = filterVertexPosition();
     vTextureCoord = filterTextureCoord();
+    vNormalizedCoord = aPosition;
   }
 `;
